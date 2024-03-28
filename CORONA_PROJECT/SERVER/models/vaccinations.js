e46935ff37f8130ce52a1d1ad323bcfa0baf61df
moria78b <164333892+moria78b@users.noreply.github.com>
2024-03-28 18:40:54 +0200

    Update vaccinations.js

--- CORONA_PROJECT/SERVER/models/vaccinations.js
+++ CORONA_PROJECT/SERVER/models/vaccinations.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('./User');
 
 const vaccinationsSchema = mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Link to the automatic code that Mongoose generated for the object   
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
     Vaccination_number: [{ type: String }],
     dateOfVaccination:[{ type: Date }]
       
