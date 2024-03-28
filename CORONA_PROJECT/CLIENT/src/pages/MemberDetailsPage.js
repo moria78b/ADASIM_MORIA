86fa77d6293fe1ecfa63d55675c53704eb5a98d2
moria78b <164333892+moria78b@users.noreply.github.com>
2024-03-28 18:44:47 +0200

    Update MemberDetailsPage.js

--- CORONA_PROJECT/CLIENT/src/pages/MemberDetailsPage.js
+++ CORONA_PROJECT/CLIENT/src/pages/MemberDetailsPage.js
@@ -6,9 +6,6 @@ const MemberDetailsPage = ({ member }) => {
             <h2>Member Details</h2>
             <p>Name: {member.firstName} {member.lastName}</p>
             <p>ID: {member.id}</p>
-            <p>Address: {member.address}</p>
-            <p>Date of Birth: {member.dateOfBirth}</p>
-            {/* Add more member details here */}
         </div>
     );
 }
