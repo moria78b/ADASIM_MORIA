3809956641498108bd64b0adbe3172100a1cd948
moria78b <164333892+moria78b@users.noreply.github.com>
2024-03-28 18:37:30 +0200

    Update index.js

--- CORONA_PROJECT/SERVER/index.js
+++ CORONA_PROJECT/SERVER/index.js
@@ -9,7 +9,6 @@ const cors = require('cors');
 
 app.use(cors())
 
-//מאזין לבקשת קבל בנתיב '/' (שורש/דף הבית)
 app.get('/', (req, res) => {
     res.send('Hello from our server!')
 })
@@ -21,9 +20,8 @@ mongoose.connect(process.env.MONGO_CONNECTION, {
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
-    console.error('Error connecting to MongoDB:', err); // Log the actual error object
+    console.error('Error connecting to MongoDB:', err); 
 });
-//מפעיל את השרת ולהקשיב לבקשות ביציאה ספציפית
 app.listen(3030, () => {
     console.log('server listening on port 3030')
 })
