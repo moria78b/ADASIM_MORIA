2aed4976ca5afad6c93440a34b899249d304eb68
moria78b <164333892+moria78b@users.noreply.github.com>
2024-03-28 18:43:07 +0200

    Update MemberCard.js

--- CORONA_PROJECT/CLIENT/src/components/MemberCard.js
+++ CORONA_PROJECT/CLIENT/src/components/MemberCard.js
@@ -5,9 +5,7 @@ const MemberCard = ({ member, onClick }) => {
         <div className="member-card" onClick={() => onClick(member.id)}>
             <h3>{member.}</h3>
             <p>ID: {member.id}</p>
-            <p>Address: {member.address}</p>
-            <p>Date of Birth: {member.}</p>
-            {/* Add more member details here */}
+           
         </div>
     );
 }
